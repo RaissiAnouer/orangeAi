78a82b19dc6912cda92be1f23f305f18e20d29d8
Raissi Anouer <raissianouer@gmail.com>
2025-10-06 21:18:24 +0100

    add the api router for gemini api controller

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\GeminiController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -13,5 +14,7 @@ Route::get('/user', function (Request $request) {
 Route::get('/user/{id}',[UserController::class,'getUser']);
 Route::post('/register',[UserController::class,'register']);
 Route::post('/login',[UserController::class,'login']);
+
+Route::post('/chat',[GeminiController::class,'chat']);
 
 
