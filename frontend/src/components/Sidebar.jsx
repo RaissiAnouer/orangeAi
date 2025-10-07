caf440aa1912b3c1bd553975ffdd1a31da4f0a32
Raissi Anouer <raissianouer@gmail.com>
2025-10-07 20:21:38 +0100

    fix the layout for sm screen

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Sidebar = () => {
   return (
-    <div className="w-[5%] h-screen bg-orange-500/20 hover:w-[20%] transition-all duration-300 ease-in-out "></div>
+    <div className="hidden md:block md:w-[5%] h-screen bg-orange-500/20 hover:w-[20%] transition-all duration-300 ease-in-out "></div>
   );
 };
 
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -6,7 +6,7 @@ const Dashboard = () => {
   return (
     <div className="flex h-screen ">
       <Sidebar />
-      <form className="relative mx-auto my-auto w-2xl shadow-md h-[80px] rounded-lg border-1 border-black/10   ">
+      <form className="relative mx-auto my-auto w-[90%]  lg:w-2xl shadow-md h-[80px] rounded-lg border-1 border-black/10   ">
         <input className="absolute top-0  left-0 right-0  px-5 pb-6 pt-3 focus:outline-none" />
         <button className="absolute bottom-0 right-0 m-2 bg-orange-500/40 rounded-full p-1">
           <img src={assets.send} className="w-7 h-7" alt="" />
