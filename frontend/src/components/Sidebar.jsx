56cd4e359d3fcd09862fec9f8931dcdc5626551b
Raissi Anouer <raissianouer@gmail.com>
2025-10-07 18:08:26 +0100

    add a sidebar to doashboard page

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Sidebar = () => {
-  return (
-    <div className="flex flex-col w-1/6 bg-[#F7F7F8] min-h-screen py-2">
-      orangeAi
-    </div>
-  );
+  return <div className="w-[5%] bg-[#FFE2C4]"></div>;
 };
 
 export default Sidebar;
