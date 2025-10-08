2adda6b152decfc0176af7bf7b1c37f1af457522
Raissi Anouer <raissianouer@gmail.com>
2025-10-08 16:55:47 +0100

    fix dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -4,12 +4,11 @@ import { assets } from "../assets/assets";
 
 const Dashboard = () => {
   return (
-    <div className="flex w-screen h-screen">
+    <div className="flex w-full h-screen overflow-hidden">
       <Sidebar />
-      <div className="flex flex-col w-full h-full m-4">
+      <div className="flex flex-col w-full  mt-3 ml-10">
         <div className="flex justify-between w-full">
           <div className="flex gap-8 items-center">
-            <img src={assets.menu} className=" w-6 h-6" alt="" />
             <h1 className="font-bold text-gray-700/80 text-xl ">
               orange
               <span
@@ -22,10 +21,10 @@ const Dashboard = () => {
           </div>
           <img src={assets.avatar} className="h-10 w-10" alt="" />
         </div>
-        <div className="relative flex justify-center items-center">
-          <div className="absolute bottom-0 w-[500px] h-[120px] bg-orange-300 opacity-40 blur-2xl rounded-full "></div>
+        <div className="relative flex justify-center items-center my-auto">
+          <div className="hidden md:block absolute bottom-0 w-[800px] h-[120px] bg-orange-300 opacity-40 blur-2xl rounded-full "></div>
 
-          <form className="relative z-10 bg-white mx-auto my-auto w-[90%] lg:w-2xl shadow-md h-[80px] rounded-lg border border-black/10 flex items-center px-4">
+          <form className="relative z-10 bg-white mx-auto my-auto w-[90%] lg:w-4xl shadow-md h-[80px] rounded-lg border border-black/10 flex items-center px-4">
             <input
               className="flex-1 h-full px-3 py-2 focus:outline-none"
               placeholder="Type a message..."
