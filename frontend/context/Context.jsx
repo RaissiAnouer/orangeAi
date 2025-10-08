fa2d2b5a1d97f60ad1dcfd988ca9d7279751c446
Raissi Anouer <raissianouer@gmail.com>
2025-10-08 19:26:02 +0100

    add function on submit handler that send message and recieve a reply

--- frontend/context/Context.jsx
+++ frontend/context/Context.jsx
@@ -1,12 +1,16 @@
+import axios from "axios";
 import React from "react";
 import { createContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 export const Context = createContext();
 
 const ContextProvider = (props) => {
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
   const navigate = useNavigate();
+  const [message, setMessage] = useState("");
+  const [reply, setReply] = useState("");
 
   const [currentState, setCurrentState] = useState("");
   const value = {
@@ -14,9 +18,33 @@ const ContextProvider = (props) => {
     backendUrl,
   };
 
+  //send message and recive a reply from gemini api
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    const response = await axios.post(
+      backendUrl + "/api/chat",
+      { message }
+      //  { headers: { Authorization: `Bearer: ${token}` } }
+    );
+    if (!response) {
+      toast.error("failled to send message");
+    } else {
+      toast.success(response.data.reply);
+    }
+  };
+
   return (
     <Context.Provider
-      value={{ currentState, setCurrentState, navigate, backendUrl }}
+      value={{
+        currentState,
+        setCurrentState,
+        navigate,
+        backendUrl,
+        message,
+        setMessage,
+        onSubmitHandler,
+      }}
     >
       {props.children}
     </Context.Provider>
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import Sidebar from "../components/Sidebar";
 import { assets } from "../assets/assets";
+import axios from "axios";
+import { Context } from "../../context/context";
+import { toast } from "react-toastify";
 
 const Dashboard = () => {
+  const { setMessage, onSubmitHandler, reply } = useContext(Context);
+
   return (
     <div className="flex w-full h-screen overflow-hidden">
       <Sidebar />
@@ -21,15 +26,25 @@ const Dashboard = () => {
           </div>
           <img src={assets.avatar} className="h-10 w-10" alt="" />
         </div>
+        <div className="w-[59vw] bg-red-500 mx-auto mt-20 rounded-full rounded-t-r-none  ">
+          {reply}
+        </div>
         <div className="relative flex justify-center items-center my-auto">
           <div className="hidden md:block absolute bottom-0 w-[800px] h-[120px] bg-orange-300 opacity-40 blur-2xl rounded-full "></div>
 
-          <form className="relative z-10 bg-white mx-auto my-auto w-[90%] lg:w-4xl shadow-md h-[80px] rounded-lg border border-black/10 flex items-center px-4">
+          <form
+            onSubmit={onSubmitHandler}
+            className="relative z-10 bg-white mx-auto my-auto w-[90%] lg:w-4xl shadow-md h-[80px] rounded-lg border border-black/10 flex items-center px-4"
+          >
             <input
+              onChange={(e) => setMessage(e.target.value)}
               className="flex-1 h-full px-3 py-2 focus:outline-none"
               placeholder="Type a message..."
             />
-            <button className="ml-2 bg-orange-500/80 rounded-full p-2">
+            <button
+              type="submit"
+              className="ml-2 bg-orange-500/80 rounded-full p-2"
+            >
               <img src={assets.send} className="w-7 h-7" alt="Send" />
             </button>
           </form>
