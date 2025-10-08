bf8481d5875264b48b6c727a9fe3540ae044cf15
Raissi Anouer <raissianouer@gmail.com>
2025-10-08 16:55:47 +0100

    fix dashboard

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { assets } from "../assets/assets";
 
 const Sidebar = () => {
+  const [openSidebar, setOpenSidebar] = useState(false);
   return (
-    <div className="hidden  md:w-[5%] h-screen bg-orange-500/80 hover:w-[20%] transition-all duration-300 ease-in-out "></div>
+    <>
+      <div
+        className={`${
+          openSidebar
+            ? " bg-orange-500/80 w-[100vw] md:w-[20vw] "
+            : "w-[6vw] md:w-[2vw]"
+        }  transition-all duration-300 ease-in-out pt-4`}
+      >
+        <img
+          src={assets.menu}
+          onClick={() => setOpenSidebar(!openSidebar)}
+          className="w-6 h-6 m-3"
+          alt=""
+        />
+      </div>
+    </>
   );
 };
 
