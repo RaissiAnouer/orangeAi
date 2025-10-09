f2fcc050595d13358b5cc3f70d6c1e001931ffe9
Raissi Anouer <raissianouer@gmail.com>
2025-10-09 17:00:44 +0100

    add terraform code that create a ressource group and virtual network

---
+++ infra/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group""orangeai"{
+    name     = "orangeai"
+    location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "vnet"{
+    name                = "exemple-network"
+    resource_group_name = azurerm_resource_group.orangeai.name
+    location            = azurerm_resource_group.orangeai.location
+    address_space       = ["10.0.0.0/16"]
+}
