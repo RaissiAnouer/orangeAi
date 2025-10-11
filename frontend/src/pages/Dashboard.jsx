fd7429a5f76f7ec07e50ce5a159c2d4b2a9bf3b2
Raissi Anouer <raissianouer@gmail.com>
2025-10-11 20:05:31 +0100

    add a worning input and remove useffect and fix mirgins problems

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -15,17 +15,15 @@ const Dashboard = () => {
     setIsEmpty,
     isEmpty,
   } = useContext(Context);
-  useEffect(() => {
-    setIsEmpty(true);
-  }, []);
 
   return (
     <div className="flex w-full h-screen overflow-hidden">
       <Sidebar />
-      <div className="flex flex-1 flex-col w-full h-full mt-3 ml-10">
+
+      <div className="flex flex-1 flex-col w-full h-full mt-3 ">
         <div className="flex justify-between w-full ">
           <div className="flex gap-8 items-center">
-            <h1 className="font-bold text-gray-700/80 text-xl ">
+            <h1 className="font-bold text-gray-700/80 text-xl pt-2 pl-8">
               orange
               <span
                 className="
@@ -35,9 +33,8 @@ const Dashboard = () => {
               </span>
             </h1>
           </div>
-          <img src={assets.avatar} className="h-10 w-10" alt="" />
+          <img src={assets.avatar} className="h-10 w-10 mr-10" alt="" />
         </div>
-
         <div className="mx-auto w-4xl max-h-[600px] overflow-y-auto space-y-8">
           {message.map((msg, idx) => (
             <div
@@ -57,28 +54,41 @@ const Dashboard = () => {
           ))}
         </div>
         <div
-          className={`flex flex-1 ${
+          className={`flex  flex-1 ${
             isEmpty === true
-              ? "items-center justify-center"
+              ? "flex-col items-center justify-center mb-40"
               : "justify-center items-end"
           } transition-all duration-300 ease-in-out `}
         >
-          <form
-            onSubmit={onSubmitHandler}
-            className=" flex items-center w-[90%] lg:w-4xl shadow-md h-[80px] rounded-lg border border-black/10  px-4"
+          <h1
+            className={`text-2xl text-center font-medium    ${
+              isEmpty === true ? "mb-10" : "hidden"
+            }`}
           >
-            <input
-              onChange={(e) => setInput(e.target.value)}
-              value={input}
-              className="flex flex-1 h-full px-3 py-2 focus:outline-none"
-              placeholder="Type a message..."
-            />
-            <button
-              type="submit"
-              className="ml-2 bg-orange-500/80 rounded-full p-2"
+            How should we get started?
+          </h1>
+          <form onSubmit={onSubmitHandler} className="flex flex-col ">
+            <div className="flex items-center w-[90%] lg:w-4xl shadow-md h-[80px] rounded-lg border border-black/10  px-4 ">
+              <input
+                onChange={(e) => setInput(e.target.value)}
+                value={input}
+                className="flex flex-1 h-full px-3 py-2 focus:outline-none"
+                placeholder="Type a message..."
+              />
+              <button
+                type="submit"
+                className="sm:ml-2 bg-orange-500/80 rounded-full px-5 sm:p-2"
+              >
+                <img src={assets.send} className="w-7 h-7" alt="Send" />
+              </button>
+            </div>
+            <p
+              className={` ${
+                isEmpty === true ? "hidden" : ""
+              } text-center text-sm font-light mb-8`}
             >
-              <img src={assets.send} className="w-7 h-7" alt="Send" />
-            </button>
+              orangeAi can make mistakes. Check important info.
+            </p>
           </form>
         </div>
       </div>
