d1c1c5110e8cd27296f7ca1f6c20dd392ae5c8df
Raissi Anouer <raissianouer@gmail.com>
2025-10-14 13:31:02 +0100

    add scroll to the new message

--- infra/main.tf
+++ infra/main.tf
@@ -22,3 +22,12 @@ resource "azurerm_virtual_network" "vnet"{
     location            = azurerm_resource_group.orangeai.location
     address_space       = ["10.0.0.0/16"]
 }
+
+resource "azurerm_subnet" "subnet"{
+  name                  = "subnet"
+  resource_group_name   = azurerm_resource_group.orangeai.name
+  virtual_network_name  = azurerm_virtual_network.orangeai.name
+  address_prefixes      = ["10.0.1.0/24"]
+
+
+}
