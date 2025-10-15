4e70f4f2452f8bf303dbc35d09035388f426c462
Raissi Anouer <raissianouer@gmail.com>
2025-10-15 17:12:09 +0100

    add google oauth

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import ContextProvider from "../context/Context.jsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const clientId = import.meta.env.VITE_CLIENT_ID;
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <ContextProvider>
-      <App />
-    </ContextProvider>
+    <GoogleOAuthProvider clientId={clientId}>
+      <ContextProvider>
+        <App />
+      </ContextProvider>
+    </GoogleOAuthProvider>
   </BrowserRouter>
 );
