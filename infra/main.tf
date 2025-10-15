75614c17cfa992f6b13a772b4bd0f11ba079cdf6
Raissi Anouer <raissianouer@gmail.com>
2025-10-15 22:29:22 +0100

    update security group

--- infra/main.tf
+++ infra/main.tf
@@ -30,10 +30,10 @@ resource "azurerm_subnet" "subnet"{
   address_prefixes      = ["10.0.1.0/24"]
 
 }
-resource "azurerm_network_security_group" "example" {
-  name                = "acceptanceTestSecurityGroup1"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_network_security_group" "asg" {
+  name                = "asg"
+  location            = azurerm_resource_group.orangeai.location
+  resource_group_name = azurerm_resource_group.orangeai.name
 
   security_rule {
     name                       = "test123"
