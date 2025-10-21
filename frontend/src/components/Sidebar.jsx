d54138fb61385bca94ea618d94ef40684706ef8a
Raissi Anouer <raissianouer@gmail.com>
2025-10-21 16:09:37 +0100

    add a profile to sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -10,27 +10,30 @@ const Sidebar = () => {
           openSidebar
             ? " bg-[#FAFBFC] w-[100vw] md:w-[17vw] "
             : "w-[6vw] md:w-[2vw] border-none"
-        }  transition-all duration-200 ease-in-out pt-4 border-r-1 border-gray-200 ml-4 `}
+        }  transition-all duration-200 ease-in-out pt-4 border-r-1 border-gray-200  `}
       >
-        <div
-          className={` ${
-            openSidebar ? "flex items-center justify-between" : ""
-          }`}
-        >
-          {openSidebar && (
-            <h1 className="font-bold text-gray-700/80 text-3xl ">
+        <div className="flex flex-col h-full  ">
+          <div className="flex items-center justify-between">
+            <h1
+              className={`font-bold text-gray-700/80 text-3xl ${
+                openSidebar ? "" : "hidden"
+              }`}
+            >
               orange
               <span className=" text-orange-500">Ai</span>
             </h1>
-          )}
-          <img
-            src={assets.menu}
-            onClick={() => setOpenSidebar(!openSidebar)}
-            className={`w-5 h-5 ${
-              openSidebar ? " " : ""
-            } transition-all duration-200 ease-in-out mx-2`}
-            alt=""
-          />
+            <img
+              src={assets.menu}
+              onClick={() => setOpenSidebar(!openSidebar)}
+              className={`w-5 h-5 ${
+                openSidebar ? " " : ""
+              } transition-all duration-200 ease-in-out mx-2`}
+              alt=""
+            />
+          </div>
+          <button className="mt-auto text-sm mb-2 w-full py-3 hover:bg-orange-500 rounded-full">
+            Anouer Raissi
+          </button>
         </div>
       </div>
     </>
