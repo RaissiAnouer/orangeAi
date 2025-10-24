2852821957fdedf285851998883b3133c566842b
Raissi Anouer <raissianouer@gmail.com>
2025-10-24 21:00:49 +0100

    add sanctum to the backend

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -9,14 +9,13 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-
+Route::middleware('auth:sanctum')->group(function(){
 
 Route::get('/user/{id}',[UserController::class,'getUser']);
-Route::post('/register',[UserController::class,'register']);
-Route::post('/login',[UserController::class,'login']);
-
 Route::post('/chat',[GeminiController::class,'chat']);
 
+});
+Route::post('/register',[UserController::class,'register']);
+Route::post('/login',[UserController::class,'login']);
 Route::post('/googleLogin',[UserController::class,'googleLogin']);
 Route::post('/googleRegister',[UserController::class,'googleRegister']);
-
