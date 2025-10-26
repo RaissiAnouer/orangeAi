7eb8bda239848181c6f97cff696d3dfec34dad8c
Raissi Anouer <raissianouer@gmail.com>
2025-10-26 14:32:45 +0100

    add event listner to close profile when click outside happens

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -85,7 +85,6 @@ const Dashboard = () => {
                 className="flex flex-1 h-full px-3 py-2 focus:outline-none"
                 placeholder="Type a message..."
               />
-              {open && <p>hello</p>}
               <button
                 type="submit"
                 className="sm:ml-2 bg-orange-500/80 rounded-full  px-5 sm:p-2"
